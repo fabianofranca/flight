0988ab1b967976ca2565845250896098851efc41
Fabiano França <email@fabianofranca.com>
2018-05-18 02:52:24 +0000

    Setup do mockwebserver para unit test

--- app/src/test/java/com/fabianofranca/flight/providers/FlightsProviderTest.kt
+++ app/src/test/java/com/fabianofranca/flight/providers/FlightsProviderTest.kt
@@ -1,45 +1,26 @@
 package com.fabianofranca.flight.providers
 
 import com.fabianofranca.flight.providers.api.Api
-import com.fabianofranca.flight.providers.api.ApiConstants
-import com.fabianofranca.flight.providers.api.RequestAdapterFactory
 import com.google.gson.Gson
 import kotlinx.coroutines.experimental.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import org.junit.Assert.assertNotNull
 import org.junit.Test
-
-import org.junit.Assert.*
-import org.junit.Before
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 
-class FlightsProviderTest {
-
-    private lateinit var api: Api
-
-    @Before
-    fun setup() {
-        api = Retrofit.Builder()
-            .baseUrl(ApiConstants.BASE_URL)
-            .addCallAdapterFactory(RequestAdapterFactory())
-            .addConverterFactory(GsonConverterFactory.create(Gson()))
-            .build()
-            .create(Api::class.java)
-    }
+class FlightsProviderTest : ProviderBaseTest<Api>(Api::class) {
 
     @Test
     fun api_shouldSearchFlights() {
 
         val provider = FlightsProviderImpl(api)
+
         var searchData: SearchData? = null
 
-        runBlocking {
-            val cal = Calendar.getInstance()
-            cal.set(Calendar.YEAR, 2018)
-            cal.set(Calendar.MONTH, Calendar.MAY)
-            cal.set(Calendar.DAY_OF_MONTH, 18)
+        mockWebServer.enqueue(MockResponse().setBody(Gson().toJson(SearchData(Data(emptySet())))))
 
-            searchData = provider.search("CNF", "VIX", cal.time).await()
+        runBlocking {
+            searchData = provider.search("", "", Calendar.getInstance().time).await()
         }
 
         assertNotNull(searchData?.data?.onwardFlights)
